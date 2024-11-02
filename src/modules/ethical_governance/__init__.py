2fad8bfc1279d36cd8263c82fea94bdad1e5953f
KOSASIH <kosasihg88@gmail.com>
2024-11-02 20:48:41 +0700

    Create __init__.py

---
+++ src/modules/ethical_governance/__init__.py
@@ -0,0 +1,6 @@
+# src/modules/ethical_governance/__init__.py
+
+from .governance import GovernanceFramework
+from .compliance import ComplianceChecker
+
+__all__ = ['GovernanceFramework', 'ComplianceChecker']
